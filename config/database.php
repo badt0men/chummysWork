a29f77ed9ec0d6464aabee9342a3f4eb1384216e
niel63 <placmedia@gmail.com>
2020-12-31 15:12:36 +0100

    update DataBase

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('us-cdbr-east-02.cleardb.com'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_55e27714b61c8ae'),
+            'username' => env('DB_USERNAME', 'b1ebd3a48748bd'),
+            'password' => env('DB_PASSWORD', 'c5e94167'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
